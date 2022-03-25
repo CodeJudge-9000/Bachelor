4710194df27079006834cba2e6035a53bd80783e
matlon411 <mathias_lendal@hotmail.dk>
2022-03-25 16:54:06 +0100

    Update dataCollector.py

--- Modules/dataCollector.py
+++ Modules/dataCollector.py
@@ -9,30 +9,55 @@ from FingerPrints import *
 from atomic_annihilator import remove_atoms
 from ase.io import Trajectory
 
-def get_raw_data_frame(directory = './data'):
-    theCwd = os.getcwd()
-    os.chdir(directory)
+def get_raw_data_frame(directory = './data') -> pd.DataFrame:
+    """ get_raw_data_frame - goes through the specified directory and collects the raw data into a singular dataframe
+    It goes for csv files and collect them to a dataFrame.
 
+    Parameters:
+    -----------
+     - `directory` [str]: The directory where we look for the csv files we want to recombine
+
+    Returns:
+    --------
+     - `df` [pd.DataFrame]: A complete dataframe
+    """
+
+    # Going down to the folder and extract all filenames
+    os.chdir(directory)
     mypath = os.getcwd()
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and ".csv" in f]
 
+    # Reading all the files and add them to a dataframe array
     dataArray = []
     for file in onlyfiles:
         df = pd.read_csv(file)
         dataArray.append(df)
-
-    dataArray2 = pd.concat(dataArray)
     os.chdir("..")
+
+    #refining the output
+    dataArray2 = pd.concat(dataArray)
     dataArray2 = dataArray2.iloc[: , 1:]
-    #dataArray2 = dataArray2.reset_index()
-
-
     df = dataArray2
     df = df.reset_index()
     return(df)
 
 
-def fill_missing(df):
+def fill_missing(df) -> pd.DataFrame:
+    """ fill_missing - A function which takes a dataframe and cleans old datapoints such that missing new_id's after removals are set if they where nan
+    fixes so older simple fingers goes from length to 4 to all 5
+    fixes so all instances will have a SCF
+
+    Parameters:
+    -----------
+     - `df` [pd.Dataframe]: the dataframe we want to clean
+    
+    Returns:
+    --------
+     - `df` [pd.Dataframe]: the cleaned dataframe
+
+    """
+
+    #Here we loop through all fingerprints and clean the simple fingerprints which was 4 long, but needed to be 5
     for g in range(0,len(df)):
         h = df.loc[g].finger
         res = h.strip('][').split(',')
@@ -44,73 +69,67 @@ def fill_missing(df):
             listen.append(0)
 
 
-        first = listen[0:1] 
-        rest = listen[1:5]
+        first = listen[:1] 
+        rest = listen[1:]
         rest.sort(reverse = True)
         listen = first + rest
 
         df = df.replace(h, str(listen))
+    
+    #Here we loop through all atoms and makes sure that we replace new id, for them which was a NAN
+    for g in range(0,len(df)):
+        h = df.loc[g].new_id
+        if isnan(h) and  df.loc[g].startsystemname == "eq_struct_6x6.traj":
+            system = read("eq_struct_6x6.traj")
+            ogId = df.loc[g].original_id
+            listt = df.loc[g].removedList
+            res = listt.strip('][').split(',')
+            listen = []
+            for a in res:
+                listen.append(int(a))
+
+            idNew = remove_atoms(system = system, atomIndex = ogId, atomRemoveIndex = listen, relax = False)
+            df.new_id[g] = idNew
         
-        for g in range(0,len(df)):
-            h = df.loc[g].new_id
-            if isnan(h) and  df.loc[g].startsystemname == "eq_struct_6x6.traj":
-                system = read("eq_struct_6x6.traj")
-                ogId = df.loc[g].original_id
-                listt = df.loc[g].removedList
-                res = listt.strip('][').split(',')
-                listen = []
-                for a in res:
-                    listen.append(int(a))
 
-                idNew = remove_atoms(system = system, atomIndex = ogId, atomRemoveIndex = listen, relax = False)
-                #df = df.replace(h, idNew)
-                df.new_id[g] = idNew
-
-        for g in range(0,len(df)):
-            h = df.loc[g].new_id
-
-            if isnan(h) and  df.loc[g].startsystemname == "eq_struct_6x6.traj":
-                system = read("eq_struct_6x6.traj")
-                ogId = df.loc[g].original_id
-                listt = df.loc[g].removedList
-                res = listt.strip('][').split(',')
-                listen = []
-                for a in res:
-                    listen.append(int(a))
-
-                idNew = remove_atoms(system = system, atomIndex = ogId, atomRemoveIndex = listen, relax = False)
-
-                #df = df.replace(h, idNew)
-                df.new_id[g] = idNew
-
-        cumList = []
-        lens = []
-        g = 0
-        # Tilføj så vi også kan se z-coordinaten af den fjernede
-        for g in range(0,len(df)):
-            newId = df.loc[g].new_id
-            newId = int(newId)
-            filename = df.loc[g].trajFile
-            if(filename[-6] == "_"):
-                filename = filename[:-6] + ".traj"
-            os.chdir("./trajec")
-            T = Trajectory(filename)
-            lens.append(len(T))
-            T = T[0]
-            cumFinger = ShortCommonFinger(system = T, index = newId)
-            cumList.append(cumFinger)
-            os.chdir("..")
+    #A lot of the data, did not have SCF, so this section is to add it to the table for the points which are missing
+    cumList = []
+    lens = []
+    for g in range(0,len(df)):
+        newId = df.loc[g].new_id
+        newId = int(newId)
+        filename = df.loc[g].trajFile
+        if(filename[-6] == "_"):
+            filename = filename[:-6] + ".traj"
+        os.chdir("./trajec")
+        T = Trajectory(filename)
+        lens.append(len(T))
+        T = T[0]
+        cumFinger = ShortCommonFinger(system = T, index = newId)
+        cumList.append(cumFinger)
+        os.chdir("..")
         df['shortFinger'] = cumList
         df['Frames'] = lens
                 
-        return(df)
-        
-def removed_categorize(df):
-        # Tilføj så vi ser om de fjernede er same side eller opposite size
+        return df
+
+def removed_categorize(df) -> pd.DataFrame:
+    """ removed_categorize - A function that takes a pandas dataframe, with our model and checks which of the neighbohrs we have removed was on the same side 
+    or the different side
+
+    Parameters:
+    -----------
+     - `df` [pd.DataFrame]: The data under consideration
+
+    Returns:
+    --------
+     - `df` [pd.DataFrame]: with the added [D,S,D,...] vector
+    """
+
+    # Tilføj så vi ser om de fjernede er same side eller opposite size
     zcoordMovedAtom = []
     zzListen = []
-
-    for g in range(0,len(df)): #len(df)
+    for g in range(0,len(df)):
             system = read("eq_struct_6x6.traj")
 
             ogId = df.loc[g].original_id
@@ -145,18 +164,15 @@ def removed_categorize(df):
                 categoryList.append(" ")
             elif(zpos > 8.5 and zcoordMovedAtom[i] > 8.5): ## Hvis begge er på toppen
                 categoryList.append("S")
-            elif(zpos < 8.5 and zcoordMovedAtom[i] < 8.5): ## Hvis begge er på toppen
+            elif(zpos < 8.5 and zcoordMovedAtom[i] < 8.5): ## Hvis begge er i bunden
                 categoryList.append("S")
             elif(zpos > 8.5 and zcoordMovedAtom[i] < 8.5): ## Hvis den ene er top og den anden er bund
                 categoryList.append("D")
             elif(zpos < 8.5 and zcoordMovedAtom[i] > 8.5): ## Hvis den ene er bund og den anden er top
                 categoryList.append("D")
 
-
         categoryVector.append(categoryList)    
 
-    ## Inversion section
-    #### For dem med original id zcoordinat over 8.5 Skal der ikke ske noget, for dem hvor den er under, skal de invertes.   
     df['neigbohrs'] = categoryVector
     return(df)
 
